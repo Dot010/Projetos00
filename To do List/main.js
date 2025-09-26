1040db21f4717f1e78fe9b03ae1e775e69a82a16
jonathan <jonathan2500@outlook.pt>
2025-09-26 15:48:27 -0300

    Adiciona projeto To Do List

---
+++ To do List/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    
+    $('#form-tarefa').on('submit', function (e) {
+        e.preventDefault();
+        const nomeTarefa = $('#nome-tarefa').val().trim();
+
+        if (nomeTarefa !== "") {
+            
+            const novaTarefa = $('<li></li>').text(nomeTarefa);
+            $('#lista-tarefas').append(novaTarefa);
+
+            $('#nome-tarefa').val('');
+        }
+    });
+
+    
+    $('#lista-tarefas').on('click', 'li', function () {
+        $(this).toggleClass('concluida');
+    });
+});
